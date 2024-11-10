42cb23ff5533d61b48d0cd7f0c8165b680f40685
cmh <charles.m.hall@gmail.com>
2024-11-10 11:53:45 -1000

    reorg tests

--- t/t.sh
+++ t/t.sh
@@ -2,15 +2,15 @@
 
 ec=0
 for t in `cat tests`; do
-  echo -n "test$t: "
-  ../gk test$t > $t 2>/dev/null
-  diff -u res$t $t &> diff$t
+  echo -n "t$t: "
+  ../gk t$t > $t 2>/dev/null
+  diff -u r$t $t &> d$t
   if [ $? -ne 0 ]; then
     ec=$((ec+1))
     echo -e "fail *****"
   else
     echo -e "pass"
-    rm -f $t diff$t
+    rm -f $t d$t
   fi
 done
 echo failed: $ec
