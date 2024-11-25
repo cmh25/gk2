48bbf035b0b5d55e196b40d7271e4741155bb2bd
Charles Hall <charles.m.hall@gmail.com>
2024-11-24 21:03:03 -1000

    header

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "repl.h"
 #include "k.h"
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+  fprintf(stderr, "gk2-v0.0.0 Copyright (c) 2023-2024 Charles Hall\n\n");
   kinit();
   if(argc>1) load(argv[1]);
   repl();
