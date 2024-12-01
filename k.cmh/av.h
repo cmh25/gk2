16fa1ec90a606e6dfbb9c9aacf25386fd5eb676a
cmh <charles.m.hall@gmail.com>
2024-11-30 14:19:44 -1000

    k.default

--- k.cmh/av.h
+++
@@ -1,5 +0,0 @@
-#ifndef AV_H
-#define AV_H
-#include "k.h"
-U over(U a,U x);
-#endif /* AV_H */
--- k.cmh/make.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cl /c k.c v.c av.c /Fe:gk /O2
-lib /nologo /out:k.lib k.obj v.obj av.obj
-exit /b
